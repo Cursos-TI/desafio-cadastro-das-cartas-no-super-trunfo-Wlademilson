926a7d30817a6935ab550b2d8e2c64e680e14292
Wlademilson <202502077149@alunos.estacio.br>
2025-04-06 00:47:54 +0000

    Supertrunfo-DesafioNovato

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 int main() {
-    char estado[3];   char estado1[3];
+    char estado[2];   char estado1[2];
     char codigo[3];   char codigo1[3];
     char cidade[21];  char cidade1[21];
     int populacao;    int populacao1;
@@ -9,16 +9,19 @@ int main() {
     float pib;        float pib1;
     int turismo;      int turismo1;
 
-
     printf("Desafio Super Trunfo - Países\n");
     printf("\n");
     printf("Cadastro de Cartas\n");
+    printf("\n");
     printf("Digite o nome do primeiro estado:\n");
-    scanf("%s", &estado);
+    scanf("%s", estado);
     printf("Digite o código:\n");
-    scanf("%s", &codigo);
+    scanf("%s", codigo);
+        //funcão setdebuf(stdin,NULL) para limpar quaisquer sobra de entrada no buffer do teclado
+    setbuf(stdin,NULL);
     printf("Digite o nome da cidade:\n");
-    scanf("%s", &cidade);
+        //funcão fgets utilizada para capturar entrada de caracteres com espaço
+    fgets(cidade, 21, stdin);
     printf("Digite a quantidade de população:\n");
     scanf("%d", &populacao);
     printf("Digite a área do estado:\n");
@@ -31,11 +34,14 @@ int main() {
     printf("\n");
     
     printf("Digite o nome do segundo estado:\n");
-    scanf("%s", &estado1);
+    scanf("%s", estado1);
     printf("Digite o código:\n");
-    scanf("%s", &codigo1);
+    scanf("%s", codigo1);
+        //funcão setdebuf(stdin,NULL) para limpar quaisquer sobra de entrada no buffer do teclado;
+    setbuf(stdin,NULL);
     printf("Digite o nome da cidade:\n");
-    scanf("%s]", &cidade1);
+        //funcão fgets utilizada para capturar entrada de caracteres com espaço
+    fgets(cidade1, 21, stdin);
     printf("Digite a quantidade de população:\n");
     scanf("%d", &populacao1);
     printf("Digite a área do estado:\n");
@@ -45,10 +51,11 @@ int main() {
     printf("Digite a quantidade de pontos turísticos:\n");
     scanf("%d", &turismo1);
 
+    printf("\n");
     printf("Carta 1:\n");
     printf("Estado:%s\n", estado);
     printf("Código:%s%s\n", estado, codigo);
-    printf("Nome da Cidade:%s\n", cidade);
+    printf("Nome da Cidade:%s", cidade);
     printf("População:%d\n", populacao);
     printf("Área:%.2fkm²\n", area);
     printf("PIB:%.2f Bilhões de reais\n", pib);
@@ -57,7 +64,7 @@ int main() {
     printf("Carta 2:\n");
     printf("Estado:%s\n", estado1);
     printf("Código:%s%s\n", estado1, codigo1);
-    printf("Nome da Cidade:%s\n", cidade1);
+    printf("Nome da Cidade:%s", cidade1);
     printf("População:%d\n", populacao1);
     printf("Área:%.2fkm²\n", area1);
     printf("PIB:%.2f Bilhões de reais\n", pib1);
